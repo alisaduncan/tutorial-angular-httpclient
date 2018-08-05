47289b52ca917413ff760f1f69db9f8642a9196f
Alisa <7586237+alisaduncan@users.noreply.github.com>
2018-08-05 12:58:39 -0500

    Enable unit tests for components

--- src/app/users/users.component.spec.ts
+++ src/app/users/users.component.spec.ts
@@ -2,15 +2,24 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 
 import { UsersComponent } from './users.component';
+import { UserService } from '../user.service';
+import { FakeUserService } from '@testing/fake-user.service';
+import { MatDialogModule } from '@angular/material';
 
-xdescribe('UsersComponent', () => {
+describe('UsersComponent', () => {
   let component: UsersComponent;
   let fixture: ComponentFixture<UsersComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        MatDialogModule,
+      ],
       declarations: [ UsersComponent ],
-      schemas: [ NO_ERRORS_SCHEMA ]
+      schemas: [ NO_ERRORS_SCHEMA ],
+      providers: [
+        { provide: UserService, useClass: FakeUserService }
+      ]
     })
     .compileComponents();
   }));
